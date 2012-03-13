7aac2024a4257b5168122ef0d2f30ba1c18adb7f
Mike Yagley <myagley@gmail.com>
2012-03-13 12:22:55 -0500

    Add support for reading datasets

--- tempodb2.php
+++ tempodb2.php
@@ -34,7 +34,7 @@ class Series {
 
 class DataPoint {
 
-    function __constuct($ts, $value) {
+    function __construct($ts, $value) {
         $this->ts = $ts;
         $this->value = $value;
     }
@@ -55,6 +55,71 @@ class DataPoint {
 }
 
 
+class DataSet {
+
+    function __construct($series, $start, $end, $data=array(), $summary=NULL) {
+        $this->series = $series;
+        $this->start = $start;
+        $this->end = $end;
+        $this->data = $data;
+        $this->summary = $summary;
+    }
+
+    function to_json() {
+        $json = array(
+            "series" => $this->series->to_json(),
+            "start" => $this->start->format("c"),
+            "end" => $this->end->format("c"),
+            "data" => array_map(array($this, $this->to_json()), $json[0]),
+            "summary" => isset($summary) ? $summary->to_json : array()
+        );
+        return $json;
+    }
+
+    static function from_json($json) {
+        $series = isset($json["series"]) ? Series::from_json($json["series"]) : NULL;
+        $start = isset($json["start"]) ? new DateTime($json["start"]) : NULL;
+        $end = isset($json["end"]) ? new DateTime($json["end"]) : NULL;
+        $data = isset($json["data"]) ? array_map("DataPoint::from_json", $json["data"]) : array();
+        $summary = isset($json["summary"]) ? Summary::from_json($json["summary"]) : NULL;
+        return new DataSet($series, $start, $end, $data, $summary);
+    }
+}
+
+
+class Summary {
+    private $data = array();
+
+    public function __get($member) {
+        if (isset($this->data[$member])) {
+            return $this->data[$member];
+        }
+        return null;
+    }
+
+    public function __isset($member) {
+        return isset($this->data[$member]);
+    }
+
+    public function __set($member, $value) {
+        $this->data[$member] = $value;
+    }
+
+    function to_json() {
+        return $data;
+    }
+
+    static function from_json($json) {
+        $summary = new Summary();
+        $data = isset($json) ? $json : array();
+
+        foreach ($data as $key => $value) {
+            $summary->__set($key, $value);
+        }
+        return $summary;
+    }
+}
+
 class TempoDB {
     const API_HOST = "api.tempo-db.com";
     const API_PORT = 443;
@@ -73,7 +138,32 @@ class TempoDB {
         return array_map("Series::from_json", $json[0]);
     }
 
-    function request($target, $method="GET", $params=array()) {
+    function read_id($series_id, $start, $end, $interval=NULL, $function=NULL) {
+        $series_type = "id";
+        $series_val = $series_id;
+        return $this->_read($series_type, $series_val, $start, $end, $interval, $function);
+    }
+
+    function read_key($series_key, $start, $end, $interval=NULL, $function=NULL) {
+        $series_type = "key";
+        $series_val = $series_key;
+        return $this->_read($series_type, $series_val, $start, $end, $interval, $function);
+    }
+
+    private function _read($series_type, $series_val, $start, $end, $interval=NULL, $function=NULL) {
+        // send GET request, formatting dates in ISO 8601
+        $params = array(
+            "start" => $start->format("c"),
+            "end" => $end->format("c"),
+            "interval" => $interval,
+            "function" => $function);
+
+        $url = "/series/" . $series_type . "/" . $series_val . "/data/";
+        $json = $this->request($url, "GET", $params);
+        return DataSet::from_json($json[0]);
+    }
+
+    private function request($target, $method="GET", $params=array()) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_USERPWD, $this->key . ":" . $this->secret);
         curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -118,14 +208,14 @@ class TempoDB {
         return array(json_decode($response, true), $http_code);
     }
 
-    function build_full_url($target, $params=array()) {
+    private function build_full_url($target, $params=array()) {
         $port = $this->port == 80 ? "" : ":" . $this->port;
         $protocol = $this->secure ? "https://" : "http://";
         $base_full_url = $protocol . $this->host . $port;
         return $base_full_url . $this->build_url($target, $params);
     }
 
-    function build_url($url, $params=array()) {
+    private function build_url($url, $params=array()) {
         $target_path = $url;
 
         if (empty($params)) {
@@ -136,7 +226,7 @@ class TempoDB {
         }
     }
 
-    function urlencode($params) {
+    private function urlencode($params) {
         return http_build_query($params, null, '&');
     }
 }
