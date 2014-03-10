7a8e365ea3fe8d3690e2b7f505cf5614228b9741
Matt Meshulam <matt.meshulam@tempo-db.com>
2014-03-10 15:08:48 -0500

    Make time ranges consistent across examples

--- examples/increment.php
+++ examples/increment.php
@@ -8,8 +8,8 @@ $tdb = new TempoDB("your-api-key", "your-api-secret");
 $series_key = "php-increment";
 
 $data = array(
-    new DataPoint(new DateTime("2012-07-04T14:00:00"), 1),
-    new DataPoint(new DateTime("2012-07-04T14:01:00"), 4)
+    new DataPoint(new DateTime("2014-02-01T12:00:00"), 1),
+    new DataPoint(new DateTime("2014-02-01T12:01:00"), 4)
 );
 
 $result = $tdb->increment_key($series_key, $data);
